2b1b7ee2ce01630cc3156379fb933390572292c9
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-05-12 10:35:52 +1000

    ; moment and server update, still havent tested

--- Server/Server.cs
+++ Server/Server.cs
@@ -78,7 +78,7 @@ namespace Startup
         app.UseStaticFiles(new StaticFileOptions
         {
             FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "images")),
-            RequestPath = "/StoreImages"
+            RequestPath = "/images"
         });
         app.UseRouting();
         app.UseCors("AllowAll");
@@ -101,13 +101,14 @@ namespace Startup
         }
     }
 }
-namespace Server.MapControllers
+namespace Server.Controllers
 {
     public class Post
     {
         public string title { get; set; }
         public string message { get; set; }
         public string picture { get; set; }
+        public IFormFile pictureFile { get; set; }
         public int likeCount { get; set; }
         public int dislikeCount { get; set; }
         public List comments { get; set;}
@@ -123,85 +124,28 @@ namespace Server.MapControllers
         public int likeCount { get; set; }
         public int dislikeCount { get; set; }
     }
-    [Route("/api/getPosts")]
-    [ApiController]
-
-    public class getPosts
+    public class options
     {
-        private class options
-        {
-            public int UserID { get; set; }
-            public int PostID { get; set; }
-        }
-        [HttpGet]
-        public async Task<ActionResult<Post>> getPosts([fromBody] options option)
+        public int UserID { get; set; }
+        public int PostID { get; set; }
+    }
+    public class handleComments
+    {
+        [Route("/api/getComments")]
+        [ApiController]
+        public async Task<ActionResult<comment>> fetchComments([FromBody] options option )
         {
             try
             {
-                string ConnectionString = ConnectionString.GetConnectionString();
-                string postStatement = ""
-                if (option.UserID !== -1 && option.PostID !== -1)
-                {
-                    postStatement = "SELECT * FROM POSTS"
-                }
-                else if (option.UserID == -1 && option.PostID !== -1) 
-                {
-                    postStatement = "SELECT * FROM POSTS WHERE PostID = @PostID"
-                }
-                else if (option.UserID !== -1 && option.PostID == -1)
-                {
-                    postStatement = "SELECT * FROM POSTS WHERE UserID = @UserID"
-                }
-
-
-                List<Post> Posts = new List<Post>();
-
-                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
-                {
-                    using (MySqlCommand command = new MySqlCommand(postStatement, connection))
-                    {
-                        if (option.UserID !== -1 && option.PostID == -1) 
-                        {
-                            command.Parameters.AddWithValue("@PostID", option.UserID);
-                        }
-                        else if (option.UserID == -1 && option.PostID !== -1)
-                        {
-                            command.Parameters.AddWithValue("@UserID", option.PostID);
-                        }
-                        
-                        
-                        connection.Open()
-
-                        MySqlDataReader reader = command.ExecuteReader()
-
-                        while (reader.read())
-                        {
-                            Post item = new Post
-                            {
-                                title = reader.GetString(reader.GetOrdinal("title")),
-                                message = reader.postID(reader.GetOrdinal("message")),
-                                picture = reader.postID(reader.GetOrdinal("picture")),
-                                likeCount = reader.GetInt32(reader.GetOrdinal("likeCount")),
-                                dislikeCount = reader.GetInt32(reader.GetOrdinal("dislikeCount")),
-                                userName = reader.postID(reader.GetOrdinal("userName")),
-                                userID = reader.GetInt32(reader.GetOrdinal("userID")),
-                                postID = reader.GetInt32(reader.GetOrdinal("postID")),                             
-                            };
-                            item.comments = await getComments(option.UserID, option.PostID)
-                            Posts.Add(item);
-                        }
-                        reader.Close();
-                        return Ok(Posts);
-                    }
-
-                }
+                List comments = await getComments(option.UserID, option.PostID);
+                return Ok(comments);
             }
             catch (System.Exception)
             {
-                return Status(500, 'Internal server error')
+                Console.WriteLine("Failed to get comments");
+                return StatusCode(500, 'Internal Server Error');
             }
         }
-
         public async Task<List<comment>> getComments(int userID, int postID)
         {
             try
@@ -234,14 +178,138 @@ namespace Server.MapControllers
                             comments.Add(comment)
                         }
                         reader.Close();
-                        return comments
+                        return comments;
                     }
                 }
             }
             catch (System.Exception)
             {
-                Console.WriteLine("Failed to get comments")
-                return ([])
+                Console.WriteLine("Failed to get comments");
+                return ([]);
+            }
+        }
+    }
+    [Route("/api/getPosts")]
+    [ApiController]
+
+    public class GetPosts : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<Post>> GetPosts([fromBody] options option)
+        {
+            try
+            {
+                string ConnectionString = ConnectionString.GetConnectionString();
+                string postStatement = "";
+                if (option.UserID !== -1 && option.PostID !== -1)
+                {
+                    postStatement = "SELECT * FROM POSTS";
+                }
+                else if (option.UserID == -1 && option.PostID !== -1) 
+                {
+                    postStatement = "SELECT * FROM POSTS WHERE PostID = @PostID";
+                }
+                else if (option.UserID !== -1 && option.PostID == -1)
+                {
+                    postStatement = "SELECT * FROM POSTS WHERE UserID = @UserID";
+                }
+
+
+                List<Post> Posts = new List<Post>();
+
+                using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+                {
+                    using (MySqlCommand command = new MySqlCommand(postStatement, connection))
+                    {
+                        if (option.UserID !== -1 && option.PostID == -1) 
+                        {
+                            command.Parameters.AddWithValue("@PostID", option.UserID);
+                        }
+                        else if (option.UserID == -1 && option.PostID !== -1)
+                        {
+                            command.Parameters.AddWithValue("@UserID", option.PostID);
+                        }
+                        
+                        
+                        connection.Open();
+
+                        MySqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.read())
+                        {
+                            Post item = new Post
+                            {
+                                title = reader.GetString(reader.GetOrdinal("title")),
+                                message = reader.postID(reader.GetOrdinal("message")),
+                                picture = reader.postID(reader.GetOrdinal("picture")),
+                                likeCount = reader.GetInt32(reader.GetOrdinal("likeCount")),
+                                dislikeCount = reader.GetInt32(reader.GetOrdinal("dislikeCount")),
+                                userName = reader.postID(reader.GetOrdinal("userName")),
+                                userID = reader.GetInt32(reader.GetOrdinal("userID")),
+                                postID = reader.GetInt32(reader.GetOrdinal("postID")),                             
+                            };
+                            item.comments = await handleComments.getComments(option.UserID, option.PostID);
+                            Posts.Add(item);
+                        }
+                        reader.Close();
+                        return Ok(Posts);
+                    }
+
+                }
+            }
+            catch (System.Exception)
+            {
+                return Status(500, 'Internal server error');
+            }
+        }
+    }
+    [Route("/api/createPost")]
+    [ApiController]
+    public class handleCreatePost
+    { 
+        static async Task<ActionResult> createPost([FromBody] Post post)
+        {
+            try
+            {
+                string extension = Path.GetExtension(post.pictureFile.FileName);
+                string imageFileName = $"{Guid.NewGuid()}.{GetFileExtension(post.pictureFile.FileName)}";
+                string imagePath = Path.Combine("images", imageFileName);
+                string fullPath = Path.Combine(Directory.GetCurrentDirectory(), "images", imageFileName);
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    await createItemRequest.Picture.CopyToAsync(stream);
+                }
+                string queryStatement = "INSERT INTO POSTS (title, message, picture, likeCount, disLikeCount, userName, userID, postID) VALUES (@title, @message, @picture, 0, 0, @userName, @userID, @postID)";
+                string connectionString = ConnectionString.GetConnectionString();
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    using (MySqlCommand command = new MySqlCommand(queryStatement, connection))
+                    {
+                        connection.Open()
+                        command.Parameters.AddWithValue("@comments", post.comments)
+                        command.Parameters.AddWithValue("@dislikeCount", post.dislikeCount)
+                        command.Parameters.AddWithValue("@likeCount", post.likeCount)
+                        command.Parameters.AddWithValue("@message", post.message)
+                        command.Parameters.AddWithValue("@picture", fullPath)
+                        command.Parameters.AddWithValue("@postID", post.postID)
+                        command.Parameters.AddWithValue("@title", post.title)
+                        command.Parameters.AddWithValue("@userID", post.userID)
+                        command.Parameters.AddWithValue("@userName", post.userName)
+                        int rowsAffected = await command.ExecuteNonQueryAsync();
+
+                        if (rowsAffected > 0){
+                            return Ok("Post made")
+                        } else {
+                            return StatusCode(500, 'Failed to create post')
+                        }
+                    }
+                }
+
+            }
+            catch (System.Exception)
+            {
+                return Status(500, 'Internal server error');
             }
         }
     }
