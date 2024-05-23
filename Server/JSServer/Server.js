ace8f517e3b15a6a8fb6a92424a5456a1bed6ade
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-05-23 10:59:40 +1000

    !!!

--- Server/JSServer/Server.js
+++ Server/JSServer/Server.js
@@ -219,7 +219,7 @@ app.post('/api/Login', async (req, res) => {
                 if (verified && newToken){
                     console.log('success', newToken);
 
-                    c
+                    res.cookie('authToken', newToken);
 
                     return res.status(200).json({...result[0], token: newToken});                    
                 } else {
